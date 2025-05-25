3d0880bef6b2849aac497b69e111e03369057651
Yuvarajapathi278 <yuvarajapathi278@gmail.com>
2025-05-25 09:44:58 +0530

    Configure for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/ 
 export default defineConfig(({ mode }) => ({
-  base: '/', // Set this for Vercel/Netlify
+  base: '/Damodara_Smart_Tech/', // Set this for GitHub Pages
   server: {
     host: "::",
     port: 8080,
